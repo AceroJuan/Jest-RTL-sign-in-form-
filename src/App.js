1504f7d15c6c4abdf6da848c75770fadb31c7f0d
Juan Acero <dev.acerojuan@gmail.com>
2021-07-06 18:31:55 -0500

    tests added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [error, setError] = useState("");
+  const [data, setData] = useState({ username: "", password: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (data.username === "" && data.password === "") {
+      return setError("username & password can't be empty");
+    }
+    if (data.username === "") {
+      return setError("username can't be empty");
+    }
+    if (data.password === "") {
+      return setError("password can't be empty");
+    }
+  };
+
+  const handleChange = (e) => {
+    setData((data) => ({ ...data, [e.target.name]: e.target.value }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1 className="capitalize">jest testing</h1>
       </header>
-    </div>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <legend className="capitalize">sign in</legend>
+          {error ? <p data-testid="errorMsg">{error}</p> : null}
+          <hr />
+          <label className="capitalize" htmlFor="username">
+            user name:
+          </label>
+          <input
+            data-testid="usernameInput"
+            type="text"
+            id="username"
+            name="username"
+            value={data.username}
+            onChange={handleChange}
+          />
+
+          <label className="capitalize" htmlFor="password">
+            password:
+          </label>
+          <input
+            data-testid="passwordInput"
+            type="password"
+            id="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+          />
+
+          <input
+            data-testid="submitBtn"
+            className="button capitalize"
+            type="submit"
+            value="sign in"
+          />
+
+          <a href="#" className="capitalize">
+            forgot password
+          </a>
+          <hr />
+          <label>new user</label>
+          <input className="button capitalize" type="submit" value="sign up" />
+        </form>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,99 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("test for app component", () => {
+  test("username input renders correctly", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const usernameInput = getByTestId("usernameInput"); // gets the input node
+
+    expect(usernameInput.value).toBe(""); // check the input's value is empty
+  });
+
+  test("password input renders correctly", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const passwordInput = getByTestId("passwordInput"); // gets the input node
+
+    expect(passwordInput.value).toBe(""); // check the input's value is empty
+  });
+
+  test("submit button renders correctly", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const submitBtn = getByTestId("submitBtn"); // gets the input node
+
+    expect(submitBtn.value).toBe("sign in"); // check the input's value is empty
+  });
+
+  test("valid userName", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const usernameInput = getByTestId("usernameInput"); // gets the input node
+
+    fireEvent.change(usernameInput, {
+      target: {
+        value: "Juan",
+      },
+    });
+
+    expect(usernameInput.value).toBe("Juan");
+  });
+
+  test("valid password", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const passwordInput = getByTestId("passwordInput"); // gets the input node
+
+    fireEvent.change(passwordInput, {
+      target: {
+        value: "Password",
+      },
+    });
+    expect(passwordInput.value).toBe("Password");
+  });
+
+  test("invalid userName && password", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const submitBtn = getByTestId("submitBtn"); // gets the input node
+
+    fireEvent.click(submitBtn); // click submit btn
+
+    const errorMsg = getByTestId("errorMsg"); // gets the error msg node
+
+    expect(errorMsg.textContent).toBe("username & password can't be empty");
+  });
+
+  test("empty userName", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const submitBtn = getByTestId("submitBtn"); // gets the input node
+
+    const passwordInput = getByTestId("passwordInput"); // gets the input node
+
+    fireEvent.change(passwordInput, {
+      target: {
+        value: "Password",
+      },
+    });
+
+    fireEvent.click(submitBtn); // click submit btn
+
+    const errorMsg = getByTestId("errorMsg"); // gets the error msg node
+
+    expect(errorMsg.textContent).toBe("username can't be empty");
+  });
+
+  test("invalid password", () => {
+    const { getByTestId } = render(<App />); // renders the component
+    const submitBtn = getByTestId("submitBtn"); // gets the input node
+
+    const usernameInput = getByTestId("usernameInput"); // gets the input node
+
+    fireEvent.change(usernameInput, {
+      target: {
+        value: "Juan",
+      },
+    });
+
+    fireEvent.click(submitBtn); // click submit btn
+
+    const errorMsg = getByTestId("errorMsg"); // gets the error msg node
+
+    expect(errorMsg.textContent).toBe("password can't be empty");
+  });
 });
